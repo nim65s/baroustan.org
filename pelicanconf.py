65a9731e42250723b476e55a27d6e5ac1f4d5788
Guilhem Saurel <guilhem.saurel@gmail.com>
2018-06-28 00:46:03 +0200

    clean stuff, fix atom

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*- #
-from __future__ import unicode_literals
+
+import os
+
+DOMAIN_NAME = os.environ.get('DOMAIN_NAME', 'local')
+HTTP = 'http' if DOMAIN_NAME == 'local' else 'https'
 
 AUTHOR = 'Guilhem Saurel'
 SITENAME = 'Baroustan'
-# SITEURL = 'https://baroustan.org'
+SITEURL = f'{HTTP}://baroustan.{DOMAIN_NAME}'
 
 PATH = 'content'
 
@@ -12,27 +15,9 @@ TIMEZONE = 'Europe/Paris'
 
 DEFAULT_LANG = 'fr'
 
-# Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
-
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+FEED_DOMAIN = SITEURL
 
 DEFAULT_PAGINATION = 3
-
-# Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
 
 THEME = 'attila'
 HEADER_COVER = 'images/header.jpg'
@@ -42,8 +27,3 @@ PLUGIN_PATHS = ['plugins', 'pelican-plugins']
 PLUGINS = ['nim_gallery', 'filetime_from_git']
 CSS_OVERRIDE = ['css/baroustan.css', 'css/magnific-popup.css']
 JS_OVERRIDE = ['js/jquery.magnific-popup.js', 'js/gallery.js']
-MARKDOWN = {
-    'extension_configs': {
-        'markdown.extensions.toc': {},
-    },
-}
